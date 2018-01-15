2041d9150daf205ad374d8db18ee72d86ec20948
GothRaven <szaghbane@gmail.com>
2018-01-15 18:40:13 +0100

    move the files in

--- pdf.c
+++ pdf.c
@@ -1,14 +1,153 @@
 #include "pdf.h"
 
-
 void pdf_object_free(pdf_object_t* o){
+  if(o == NULL)
+    exit(EXIT_FAILURE);
+  if(o->type == string)
+      free(o->string);
+  else if(o->type == nom)
+    free(o->nom);
+  else if(o->type == stream)
+    pdf_stream_free(o);
+  else if(o->type == tab)
+    pdf_tab_free(o);
+  else if(o->type == dict)
+    pdf_dict_free(o);
+}
 
+void pdf_stream_free(pdf_object_t* o){
+  if(o != NULL && o->type == stream){
+    pdf_dict_free(o->stream->dict);
+    free(o->stream->valeur);
+  }
+}
+
+void pdf_dict_free(pdf_object_t* o){
+  if(o != NULL && o->type == dict){
+    pdf_dict_t* tet = o->dict;
+    while(tet != NULL){
+      free(tet->cle);
+      pdf_object_free(tet->valeur);
+      tet = tet->next;
+    }
+  }
+}
+
+void pdf_tab_free(pdf_object_t* o){
+  if(o != NULL && o->type == tab){
+    for(int i = 0; i < o->tab->size; i++){
+      free(o->tab->tab[i]);
+    }
+  }
 }
 
 pdf_object_t* pdf_null(pdf_object_t* o){
-  if(o==NULL){
+  if(o==NULL)
     o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
-  } else {
-    //
-  }
+  else
+    pdf_object_free(o);
+  o->type = null;
+  return o;
+}
+
+pdf_object_t* pdf_bool(pdf_object_t* o, bool b){
+  if(o==NULL)
+    o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
+  else
+    pdf_object_free(o);
+  o->type = boolean;
+  o->boolean = b;
+  return o;
+}
+
+pdf_object_t* pdf_int(pdf_object_t* o, int i){
+  if(o==NULL)
+    o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
+  else
+    pdf_object_free(o);
+  o->type = entier;
+  o->entier = i;
+  return o;
+}
+
+pdf_object_t* pdf_real(pdf_object_t* o, float f){
+  if(o==NULL)
+    o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
+  else
+    pdf_object_free(o);
+  o->type = reel;
+  o->reel = f;
+  return o;
+}
+
+pdf_object_t* pdf_reference(pdf_object_t* o, unsigned int id){
+  if(o==NULL)
+    o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
+  else
+    pdf_object_free(o);
+  o->type = ref;
+  o->ref = id;
+  return o;
+}
+
+pdf_object_t* pdf_name(pdf_object_t* o, const char* n){
+  if(o==NULL)
+    o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
+  else
+    pdf_object_free(o);
+  o->type = nom;
+  o->nom = strdup(n);
+  return o;
+}
+
+pdf_object_t* pdf_string(pdf_object_t* o, const char* s){
+  if(o==NULL)
+    o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
+  else
+    pdf_object_free(o);
+  o->type = string;
+  o->string = strdup(s);  
+  return o;
+}
+
+pdf_object_t* pdf_stream(pdf_object_t* o, size_t sz, void* data){
+  if(o==NULL)
+    o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
+  else
+    pdf_object_free(o);
+  o->type = stream;
+  o->stream = (pdf_stream_t*)malloc(1 * sizeof(pdf_stream_t));
+  pdf_object_t* dict = NULL;
+  o->stream->dict = pdf_dictionary(dict);
+  pdf_int(pdf_dict_get(o->stream->dict, "Lenght"), sz);
+  o->stream->valeur = data;
+  return o;
+}
+
+pdf_object_t* pdf_array(pdf_object_t* o, size_t n){
+  if(o==NULL)
+    o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
+  else
+    pdf_object_free(o);
+  o->type = tab;
+  o->tab = (pdf_tab_t*)malloc(1 * sizeof(pdf_tab_t));
+  o->tab->tab = (pdf_object_t*)malloc(n * sizeof(pdf_object_t));
+  for (int i = 0; i < n; ++i)
+    o->tab->tab[i] = pdf_null(NULL);
+  o->tab->size = n;
+  return o;
+}
+
+pdf_object_t* pdf_dictionary(pdf_object_t* o){
+  if(o==NULL)
+    o = (pdf_object_t*)malloc(1 * sizeof(pdf_object_t));
+  else
+    pdf_object_free(o);
+  o->type = dict;
+  o->dict = NULL;
+  return o; 
+}
+
+pdf_object_t* pdf_stream_from_file(pdf_object_t* o, const char* fname){
+  
 }
--- pdf.h
+++ pdf.h
@@ -10,7 +10,7 @@ typedef struct object_stream pdf_stream_t;
 
 struct object_dict
 {
-	char* nom;
+	char* cle;
 	pdf_object_t* valeur;
 	struct object_dict* next;
 };
@@ -18,12 +18,12 @@ struct object_dict
 struct object_tab
 {
 	pdf_object_t** tab;
-	int size;
+	size_t size;
 };
 
 struct object_stream
 {
-	struct object_dict* dict;
+	struct pdf_object_t* dict;
 	void* valeur;
 };
 
@@ -51,7 +51,34 @@ struct object
 		struct object_stream* stream;
 		struct object_tab* tab;
 		struct object_dict* dict;
-	}
+	};
 };
 
+void pdf_object_free(pdf_object_t* o);
+void pdf_stream_free(pdf_object_t* o);
+void pdf_dict_free(pdf_object_t* o);
+void pdf_tab_free(pdf_object_t* o);
+pdf_object_t* pdf_null(pdf_object_t* o);
+pdf_object_t* pdf_bool(pdf_object_t* o, bool b);
+pdf_object_t* pdf_int(pdf_object_t* o, int i);
+pdf_object_t* pdf_real(pdf_object_t* o, float f);
+pdf_object_t* pdf_reference(pdf_object_t* o, unsigned int id);
+pdf_object_t* pdf_name(pdf_object_t* o, const char* n);
+pdf_object_t* pdf_string(pdf_object_t* o, const char* s);
+pdf_object_t* pdf_stream(pdf_object_t* o, size_t sz, void* data);
+pdf_object_t* pdf_array(pdf_object_t* o, size_t n);
+pdf_object_t* pdf_dictionary(pdf_object_t* o);
+
+
+
+/*
+  "font_size": 11,
+  "ignored_packages":
+  [
+    "Vintage"
+  ],
+  "tab_size": 2,
+    "translate_tabs_to_spaces": true,
+    "keys": ["alt+shift+c"], "command": "reindent", "args": {"single_line": false}
+*/
 #endif
